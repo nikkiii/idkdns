16a015258900a370c8ff2c691e9a779f7a4dff6c
Nikki <nikki@nikkii.us>
2014-05-29 02:35:00 -0400

    Remove extra file and add missing title attribute change

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<title>Start Bootstrap - SB Admin Version 2.0 Demo</title>
+		<title>@yield('title', Config::get('app.name'))</title>
 
 		{{ HTML::style('/css/bootstrap.min.css') }}
 		{{ HTML::style('/font-awesome/css/font-awesome.css') }}
